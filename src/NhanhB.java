1c3d3553ef21a5a297192c342f8bd061a7f1fe73
Tăng Khải Minh <tangkhaiminh2002@gmail.com>
2023-11-25 23:00:46 +0700

    Add attribute

--- src/NhanhB.java
+++ src/NhanhB.java
@@ -1,5 +1,24 @@
 public class NhanhB {
+    private String name;
+    private String address;
+    private int age;
+
     public static void main(String[] args) {
         System.out.println("Hello Nhánh B!");
+    }
+
+    public NhanhB(String name, String address, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "NhanhB{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
     }
 }

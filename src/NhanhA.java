4b95eebafe92457ffbca5f32588ace07770f5e35
Tăng Khải Minh <tangkhaiminh2002@gmail.com>
2023-11-25 23:24:43 +0700

    nhanh B ok

--- src/NhanhA.java
+++ src/NhanhA.java
@@ -1,4 +1,7 @@
+import java.util.List;
+
 public class NhanhA {
+    private List<String> names;
     private String dayLaNhanhA;
     public static void main(String[] args) {
         System.out.println("Hello");
